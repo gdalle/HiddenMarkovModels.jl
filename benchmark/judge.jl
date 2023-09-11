991faee96229a7664c6d357b957556267fce82f9
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-09-11 20:47:16 +0200

    Switch judge order (#39)

--- benchmark/judge.jl
+++ benchmark/judge.jl
@@ -1,13 +1,16 @@
+using Pkg
+Pkg.activate(@__DIR__)
+
 using BenchmarkTools
 using PkgBenchmark
 
 pkg = dirname(@__DIR__) # this git repo
-baseline = "12fa976"  # commit id
-target = "1b9a7f1"  # commit id
+baseline = "04ee332"  # commit id
+target = "859c7b0"  # commit id
 
 results_baseline = benchmarkpkg(pkg, baseline; verbose=true, retune=false)
 results_target = benchmarkpkg(pkg, target; verbose=true, retune=false)
 
-judgement = judge(results_baseline, results_target, minimum)
+judgement = judge(results_target, results_baseline, minimum)
 
 export_markdown(joinpath(@__DIR__, "benchmark_judgement.md"), judgement)
