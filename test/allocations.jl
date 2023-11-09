0142f6d81e0a6dec822ba564ba0878329b0c6651
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-11-09 16:35:15 +0100

    Reactivate sparse allocation test

--- test/allocations.jl
+++ test/allocations.jl
@@ -45,7 +45,7 @@ end
 
 @testset "Normal sparse" begin
     # see https://discourse.julialang.org/t/why-does-mul-u-a-v-allocate-when-a-is-sparse-and-u-v-are-views/105995
-    @test_skip test_allocations(rand_gaussian_hmm_1d(N; sparse_trans=true); T)
+    test_allocations(rand_gaussian_hmm_1d(N; sparse_trans=true); T)
 end
 
 @testset "LightDiagNormal" begin
