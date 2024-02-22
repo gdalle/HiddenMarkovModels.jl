0623e1ea9b1df31cc22ffc53d48dbdf6ade6c18e
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-02-22 19:10:48 +0100

    Remove Pkg business

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,8 +1,3 @@
-using Pkg
-Pkg.activate(@__DIR__)
-Pkg.develop(; path=joinpath(@__DIR__, ".."))
-Pkg.develop(; path=joinpath(@__DIR__, "..", "libs", "HMMBenchmark"))
-
 using BenchmarkTools
 using HMMBenchmark
 using Random
