6b2a3d4952d2c36e79a011f759fa31fa975d1b5c
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-10-01 16:32:24 +0200

    Remove unused predict

--- src/inference/predict.jl
+++ src/inference/predict.jl
@@ -8,10 +8,3 @@ function predict_next_state!(
     mul!(next_state_marginals, transpose(trans), current_state_marginals)
     return next_state_marginals
 end
-
-function predict_next_state(
-    hmm::AbstractHMM, current_state_marginals::AbstractVector{<:Real}, control=nothing
-)
-    next_state_marginals = similar(current_state_marginals)
-    return predict_next_state!(next_state_marginals, hmm, current_state_marginals, control)
-end
